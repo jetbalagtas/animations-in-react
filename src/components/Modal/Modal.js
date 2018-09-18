b78003e4511a9ddb804f2121c96cf118ac10d291
Jet Balagtas <jetbalagtas@gmail.com>
2018-09-18 19:08:07 -0400

    wrapping the Transition component inside of a functional component, the Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
-const modal = (props) => {
-  const cssClasses = [
-    'Modal',
-    props.show === 'entering' ? 'ModalOpen' :
-    props.show === 'exiting' ? 'ModalClosed' : null
-  ];
+const modal = (props) => (
+    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+      {state => {
+        const cssClasses = [
+          'Modal',
+          state === 'entering' ? 'ModalOpen' :
+          state === 'exiting' ? 'ModalClosed' : null
+        ];
 
-  return (
-    <div className={cssClasses.join(' ')}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
+        return (
+          <div className={cssClasses.join(' ')}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
   );
-};
 
 export default modal;
