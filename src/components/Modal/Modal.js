0df88d6613b6bfc19c7f1ad1927068ce370653dc
Jet Balagtas <jetbalagtas@gmail.com>
2018-09-19 16:24:41 -0400

    use CSSTransition component instead of manually joining classes based on state

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Transition from 'react-transition-group/Transition';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 import './Modal.css';
 
@@ -9,24 +9,19 @@ const animationTiming = {
 };
 
 const modal = (props) => (
-    <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
-      {state => {
-        const cssClasses = [
-          'Modal',
-          state === 'entering' ? 'ModalOpen' :
-          state === 'exiting' ? 'ModalClosed' : null
-        ];
-
-        return (
-          <div className={cssClasses.join(' ')}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
-      }}
-    </Transition>
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      classNames='fade-slide'>
+      <div className='Modal'>
+        <h1>A Modal</h1>
+        <button className='Button' onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 
 export default modal;
