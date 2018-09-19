0fa67d5aa3eeca9b1a63454d787972e757d8d35e
Jet Balagtas <jetbalagtas@gmail.com>
2018-09-19 16:07:43 -0400

    looking at Transition events

--- src/App.js
+++ src/App.js
@@ -30,7 +30,18 @@ class App extends Component {
           Toggle
         </button>
         <br />
-        <Transition in={this.state.showBlock} timeout={1000} mountOnEnter unmountOnExit>
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
+          >
           {state => (
             <div
               style={{
